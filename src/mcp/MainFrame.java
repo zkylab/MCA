59b2ae5560307333f122db697e821cd6f39d7eef
zkylab <zkylab@gmail.com>
2017-04-07 21:40:08 +0300

    deneme

--- src/mcp/MainFrame.java
+++ src/mcp/MainFrame.java
@@ -9,10 +9,13 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.border.LineBorder;
 import java.awt.Color;
+import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -47,7 +50,26 @@ public class MainFrame extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new LineBorder(new Color(0, 0, 0), 3));
-		panel.setBounds(158, 78, 319, 154);
+		panel.setBounds(105, 70, 473, 276);
 		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Kullanıcı Adı : ");
+		lblNewLabel.setBounds(56, 55, 98, 15);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(175, 49, 122, 27);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(175, 88, 122, 27);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Password :");
+		lblPassword.setBounds(54, 82, 87, 15);
+		panel.add(lblPassword);
 	}
 }
